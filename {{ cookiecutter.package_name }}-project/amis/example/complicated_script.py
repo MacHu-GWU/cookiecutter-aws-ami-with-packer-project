c7f29dbe67d37fb9984b6bbb5e7627b7920c6962
Sanhe Hu <husanhe@gmail.com>
2023-03-19 16:02:58 -0400

    c

--- {{ cookiecutter.package_name }}-project/amis/example/complicated_script.py
+++ {{ cookiecutter.package_name }}-project/amis/example/complicated_script.py
@@ -2,9 +2,9 @@
 
 print("========== Complicated Script starts here ==========")
 
-import {{ cookiecutter.package_name }}
+from {{ cookiecutter.package_name }} import __version__
 
-print(f"{{ cookiecutter.package_name }} version: {{{ cookiecutter.package_name }}.__version__}")
+print(f"{{ cookiecutter.package_name }} version: {__version__}")
 
 from {{ cookiecutter.package_name }}.config.init import config
 
